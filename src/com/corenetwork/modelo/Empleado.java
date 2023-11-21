416b7d17a4007c200736e170a4b44529eb1d68d3
Tzveta44 <147422641+Tzveta44@users.noreply.github.com>
2023-11-21 15:52:51 +0100

    Update Empleado.java

--- src/com/corenetwork/modelo/Empleado.java
+++ src/com/corenetwork/modelo/Empleado.java
@@ -1,2 +1,26 @@
 package com.corenetwork.modelo;public class Empleado {
+   private int id;
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "id=" + id +
+                '}';
+    }
+
+    public Empleado() {
+    }
+
+    public Empleado(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
 }
